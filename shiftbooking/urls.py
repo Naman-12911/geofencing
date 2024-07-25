9b52302681c37bd2bc5921e73eea6c0cb2b6106e
Naman-12911 <namangarg820@gmail.com>
2024-07-25 16:12:59 +0530

    add urls

--- shiftbooking/urls.py
+++ shiftbooking/urls.py
@@ -4,7 +4,11 @@ from .views import *
 
 urlpatterns = [
      path('shift-booking/', ShiftBookingAPIview.as_view(),name="ShiftBookingAPIview"),
+     path('shift-booking/<int:pk>/', ShiftBookingAPIview.as_view(),name="ShiftBookingAPIview"),
      path('check-in-check-out/', CheckInCheckOutAPIview.as_view(),name="CheckInCheckOutAPIview"),
+     path('check-in-check-out/<int:pk>/', CheckInCheckOutAPIview.as_view(),name="CheckInCheckOutAPIview"),
      path('geo-fencing/', GeoFencingAPIview.as_view(),name="GeoFencingAPIview"),
+     path('geo-fencing/<int:pk>/', GeoFencingAPIview.as_view(),name="GeoFencingAPIview"),
      path('notification/', NotificationAPIview.as_view(),name="NotificationAPIview"),
+     path('notification/<int:pk>/', NotificationAPIview.as_view(),name="NotificationAPIview"),
 ]
