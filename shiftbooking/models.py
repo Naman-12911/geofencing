68d078c4e85d4be740c2db7f243f32866b9ecc64
Naman-12911 <namangarg820@gmail.com>
2024-08-10 18:49:31 +0530

    remove update

--- shiftbooking/models.py
+++ shiftbooking/models.py
@@ -8,7 +8,6 @@ class ShiftBooking(models.Model):
     shedule = models.DateField(null=True)
     shedule_confirm = models.BooleanField(default=False)
     created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now_add=True)
 
 
 class CheckInCheckOut(models.Model):
@@ -16,14 +15,13 @@ class CheckInCheckOut(models.Model):
     check_in_time = models.DateTimeField(null=True,blank=True)
     check_out_time = models.DateTimeField(null=True,blank=True)
     created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now_add=True)
-
+   
 
 class GeoFencing(models.Model):
     user = models.ForeignKey(User,models.CASCADE,null=True,blank=True)
     cordinates = models.TextField(null=True,blank=True)
     created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now_add=True)
+
 
 
 class Notification(models.Model):
@@ -31,4 +29,4 @@ class Notification(models.Model):
     message = models.CharField(max_length=100,null=True,blank=True)
     title = models.CharField(max_length=100,null=True,blank=True)
     created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    
